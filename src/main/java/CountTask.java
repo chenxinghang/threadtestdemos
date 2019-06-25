331476bafdc3f8526c5823f28dd35e773a4e1dad
陈兴航 <chenxinghang@souche.com>
2019-06-25 22:17:42 +0800

    count task demo add

---
+++ src/main/java/CountTask.java
@@ -0,0 +1,67 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.ForkJoinTask;
+import java.util.concurrent.RecursiveTask;
+
+/**
+ * User: LEON
+ * <p>
+ * Date: 2019-06-22
+ * <p>
+ * Sign: My name is leon 兴趣使然写代码的人 ≡(▔﹏▔)≡ ≧ω≦
+ **/
+//                             递归task
+public class CountTask extends RecursiveTask<Long> {
+
+    private static final int THRESHOLD = 10000;
+
+    private long start;
+
+    private long end;
+
+    public CountTask(long start, long end) {
+        this.start = start;
+        this.end = end;
+    }
+
+    protected Long compute() {
+        long sum = 0;
+        boolean canCompute = (end - start) <THRESHOLD;
+        if (canCompute) {
+            for (long i = start; i < end; i++) {
+                sum += i;
+            }
+        }else {
+            long step = (start + end) / 100;
+            ArrayList<CountTask> subTasks = new ArrayList<>();
+            long pos = start;
+            for (int i=0;i<100;i++) {
+                long lastOne = pos + step;
+                if (lastOne > end)lastOne = end;
+                CountTask subTask = new CountTask(pos,lastOne);
+                pos += step+1;
+                subTasks.add(subTask);
+                subTask.fork();
+            }
+            for(CountTask t: subTasks) {
+               sum += t.join();
+            }
+        }
+        return sum;
+    }
+
+    public static void main(String ... args) {
+        ForkJoinPool forkJoinPool = new ForkJoinPool();
+        CountTask   task = new CountTask(0,200000L);
+        ForkJoinTask<Long> result = forkJoinPool.submit(task);
+        try {
+            long res = result.get();
+            System.out.println(res);
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
